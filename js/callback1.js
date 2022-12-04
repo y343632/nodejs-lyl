052ed25eb16260ab087562a1aa26560be63836a5
ashley <ashleylai58@gmail.com>
2022-12-04 15:38:08 +0800

    fix callback1

--- js/callback1.js
+++ js/callback1.js
@@ -1,10 +1,13 @@
-let doWork = function (job, timer) {
+let doWork = function (job, timer, cb) {
   setTimeout(() => {
     let now = new Date();
-    console.log(`完成工作 ${job} at ${now.toISOString()}`);
+    cb(null, `完成工作 ${job} at ${now.toISOString()}`);
   }, timer);
 };
 
 let now = new Date();
 console.log(`工作開始 at ${now.toISOString()}`);
-doWork('刷牙', 3000);
+// 刷牙 3 秒鐘 -> 吃早餐 5 秒鐘 -> 寫功課 3 秒鐘
+doWork('刷牙', 3000, (err, data) => {
+  console.log(data);
+});
