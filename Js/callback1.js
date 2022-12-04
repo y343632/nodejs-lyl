c475520525a807586eee8addcdb481cb7dd47169
y343632 <y343632@gmail.com>
2022-12-04 16:12:18 +0800

    push

--- Js/callback1.js
+++ Js/callback1.js
@@ -11,7 +11,7 @@ let doWork = function (job, timer, cb) {
   doWork('刷牙', 3000, (err, data) => {
     console.log(data);
   });
-  doWork('吃早餐', 5000, (err, data) => {
+  doWork('吃早餐!', 5000, (err, data) => {
     console.log(data);
   });
   doWork('寫功課', 3000, (err, data) => {
