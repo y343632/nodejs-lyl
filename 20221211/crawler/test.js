bf68206b10b4202982fecf5319d1edbb265c6dda
y343632 <y343632@gmail.com>
2022-12-17 11:36:42 +0800

    push

---
+++ 20221211/crawler/test.js
@@ -0,0 +1,21 @@
+//獲取客戶端
+const mysql2 =require('mysql2/promise');
+
+//創建與數據庫的連接
+(async function data(){
+let connection = await mysql2.createConnection({
+    host:"localhost",
+    port: 3306, 
+    user:"admin",
+   
+    database:"stock_mfee31",
+}) ;
+
+//簡單查詢
+
+let result= await connection.query("SELECT * FROM `stocks`");
+let data=result[0];
+// console.log (result);
+console.log (data);
+connection.end();
+})();
